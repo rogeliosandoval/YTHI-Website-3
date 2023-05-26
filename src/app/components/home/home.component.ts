d9522c448ee30a96c65c0bad25fea87c5d202423
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2023-05-25 23:11:44 -0500

    add card data for home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,5 +7,9 @@ import { Component } from "@angular/core";
 })
 
 export class HomeComponent {
-
+    cards = [
+        { icon: 'access_time', title: 'Fast Delivery', text: 'Weekend inspections available and 24hr or less delivery of inspection reports'},
+        { icon: 'attach_money', title: 'Work Smarter', text: 'Online ordering and our use of state-of-the-art equipment will save you time and money' },
+        { icon: 'mood', title: 'Licensed and Certified', text: 'We are in good standing with InterNACHI, SABOR, TPREIA, and TREC'}
+    ]
 }
