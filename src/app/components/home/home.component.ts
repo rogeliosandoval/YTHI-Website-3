4355547c4101f9fb490ac8fed94a47d0938174e9
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2023-06-04 17:29:30 -0500

    add list to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,4 +12,17 @@ export class HomeComponent {
         { icon: 'attach_money', title: 'Work Smarter', text: 'Online ordering and our use of state-of-the-art equipment will save you time and money' },
         { icon: 'mood', title: 'Licensed and Certified', text: 'We are in good standing with InterNACHI, SABOR, TPREIA, and TREC'}
     ]
+
+    certifiedList =[
+        'Safe Practices for the Home Inspector course.',
+        '25 Standards Every Inspector Should Know course.',
+        'Residential Plumbing Inspection course.',
+        'Roof Inspection course.',
+        'HVAC Inspection course.',
+        'Exterior Inspection course.',
+        'Attic, Insulation, Ventilation and Interior Inspection course.',
+        'Deck Inspection course.',
+        'Moisture Intrusion Inspection course.',
+        'Inspecting Foundation Walls and Piers course.'
+    ]
 }
