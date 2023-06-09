c1bbd6822027c8ebb258f338215a6ff04f7140e9
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2023-06-08 23:25:51 -0500

    add logic to the home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,15 +9,17 @@ import { Component } from "@angular/core";
 export class HomeComponent {
 
     constructor(){
-        // this.loopTestimonial();
+        this.loopTestimonial();
     }
 
     public testimonials = [
         'Always a great experience. Always take great care of my clients with each and every inspection.',
         'I would recommend your inspection be conducted by them! Thorough and report submitted in a timely manner!',
+        'Same day inspection report, fast and efficient.',
         'I\'m a Realtor and Your Time Home Inspections had great availability and came out quick to inspect and did a great job and had a keen eye. I will be using them again.',
         'James was very professional. He was able to answer all my questions. Very pleased with their services. Would definitely recommend the company.',
-        'Dope service. Third time using them.'
+        'Dope service. Third time using them.',
+        'I always request Luis, he is great at explaining the inspection process to my clients. They have a great team.'
     ]
 
     public i = 0;
@@ -45,6 +47,24 @@ export class HomeComponent {
         'Inspecting Foundation Walls and Piers course.'
     ]
 
+    public certifications = [
+        'assets/icons/c1.png',
+        'assets/icons/c2.png',
+        'assets/icons/c3.png',
+        'assets/icons/c4.png',
+        'assets/icons/c5.png',
+        'assets/icons/c6.png',
+        'assets/icons/c7.png',
+        'assets/icons/c8.png',
+        'assets/icons/c9.png',
+        'assets/icons/c10.png',
+        'assets/icons/c11.png',
+        'assets/icons/c12.png',
+        'assets/icons/c13.png',
+        'assets/icons/c14.png',
+        'assets/icons/c15.png'
+    ]
+
     loopTestimonial(): void {
         setTimeout(() => {
             this.nextTestimonial();
@@ -57,7 +77,7 @@ export class HomeComponent {
     }
 
     nextTestimonial(): void {
-        if (this.i <= 3) {
+        if (this.i <= 5) {
             this.i = this.i + 1;
         } else {
             this.i = 0;
