5e075687cabcf73c7aba7caa5582853287dae14e
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2023-07-04 15:50:55 -0500

    create guarantee dialog

---
+++ src/app/dialogs/guarantee/guarantee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'guarantee',
+    templateUrl: './guarantee.component.html',
+    styleUrls: ['./guarantee.component.scss']
+})
+
+export class GuaranteeDialog {
+
+}
