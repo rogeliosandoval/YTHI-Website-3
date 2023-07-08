8a66ae7ad3d65d372d2ff8b31a7e473fbf1298d0
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2023-07-07 23:23:22 -0500

    update the doom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { fader } from './route-animations';
 
 @Component({
   selector: 'app-root',
+  animations: [fader],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ythi-website-3';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
