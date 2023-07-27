f8ee4c9c11bdc09a41614b04c2612098ccf94f1f
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2023-07-26 23:03:07 -0500

    add type

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component } from "@angular/core";
 })
 
 export class FooterComponent {
-    public socialIcons = [
+    public socialIcons: any[] = [
         'facebook', 'instagram', 'google', 'youtube', 'linkedin'
     ]
 }
