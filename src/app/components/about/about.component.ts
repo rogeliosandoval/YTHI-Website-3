a1b2bd7f6d68bb21cd6937021ae12941278d8bf8
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2023-08-09 22:19:46 -0500

    update the doom

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -4,7 +4,6 @@ import { Component } from "@angular/core";
     selector: 'about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.scss']
-
 })
 
 export class AboutComponent {
