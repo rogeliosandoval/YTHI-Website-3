073f5edbd7f62aeb87b55aa6ba37a91c5f1e01d7
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2023-11-20 23:11:23 -0600

    update the about page

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { MatDialog } from '@angular/material/dialog';
+import { GuaranteeDialog } from "src/app/dialogs/guarantee/guarantee.component";
 
 @Component({
     selector: 'about',
@@ -7,17 +9,25 @@ import { Component } from "@angular/core";
 })
 
 export class About {
+
+    constructor(
+        private matDialog: MatDialog
+    ){}
     public title: string = '';
     public name: string = '';
     public supportTeam: Array<any> = [
         {name: 'elvia', displayName: 'Elvia Williams'},
         {name: 'kalen', displayName: 'Kalen Bahlman'},
         {name: 'roger', displayName: 'Roger Sandoval'}
-    ];
+    ]
 
     public inspectors: Array<any> = [
         {name: 'james', displayName: 'James Williams', trec: '21507'},
         {name: 'luis', displayName: 'Luis Vargas', trec: '23535'},
         {name: 'robbie', displayName: 'Robbie Spencer', trec: '23968'}
-    ];
+    ]
+
+    public openGuarantee(): void {
+        this.matDialog.open(GuaranteeDialog);
+    }
 }
