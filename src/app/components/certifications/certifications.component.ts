1316164c9e4127e469ce0dcb3a9e7be744d1642f
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2023-12-04 23:20:41 -0600

    create cert component

---
+++ src/app/components/certifications/certifications.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'certifications',
+  templateUrl: './certifications.component.html',
+  styleUrls: ['./certifications.component.scss']
+})
+
+export class CertificationsComponent {
+  public certifications = [
+    'assets/icons/c1.png',
+    'assets/icons/c2.png',
+    'assets/icons/c3.png',
+    'assets/icons/c4.png',
+    'assets/icons/c5.png',
+    'assets/icons/c6.png',
+    'assets/icons/c7.png',
+    'assets/icons/c8.png',
+    'assets/icons/c9.png',
+    'assets/icons/c10.png',
+    'assets/icons/c11.png',
+    'assets/icons/c12.png',
+    'assets/icons/c13.png',
+    'assets/icons/c14.png',
+    'assets/icons/c15.png'
+  ]
+}
